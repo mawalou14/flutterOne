08a49ec3f2401e29d557405ef57abc6e5e2b0162
mawalou14 <awalou213@gmail.com>
2023-10-21 12:39:23 +0100

    configured firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,7 @@ void main() {
   runApp(const MyApp());
 }
 
-void test() {
-  ['Foo', 'Bar', 'Baz'];
-}
+void test() {}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -20,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter demo'),
     );
   }
 }
